6dca0e7935c217fa777017082168584739a562ba
Sapphirenatalie <168541642+Sapphirenatalie@users.noreply.github.com>
2024-07-15 23:01:10 +0300

    Update true_math.py

--- true_math.py
+++ true_math.py
@@ -3,10 +3,8 @@ from math import inf
 
 def divide(first, second):
     try:
-        result = first / second
-        if first > 0 and second > 0:
-            if result > 0:
-                return result
+        result = first / second        
+        return result
     except ZeroDivisionError:
         return inf
 
