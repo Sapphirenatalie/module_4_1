94b7ecee9ad02375c0642de3d4e582cc151c340a
Sapphirenatalie <168541642+Sapphirenatalie@users.noreply.github.com>
2024-07-15 23:00:53 +0300

    Update fake_math.py

--- fake_math.py
+++ fake_math.py
@@ -4,9 +4,7 @@ from math import inf
 def divide(first, second):
     try:
         result = first / second
-        if first > 0 and second > 0:
-            if result > 0:
-                return result
+        return result
     except ZeroDivisionError:
         return 'Ошибка'
 
